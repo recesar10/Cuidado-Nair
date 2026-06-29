4e6ffc6af1f76987e806c5cf313ca5bd43082153
recesar10 <recesar@gmail.com>
2026-06-28 22:05:23 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='cuidado-nair-v13';
-const FILES=['./?v=12','./index.html?v=12','./styles.css?v=12','./app.js?v=12','./manifest.json?v=12','./icon-192.png','./icon-512.png','./alarm.wav'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=12'))))});
